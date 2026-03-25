4a7c63e5ca851fee6e3b4eadef834305d91fe8ab
marcosluna87.dev <marcosluna87.dev@gmail.com>
2026-03-25 11:18:14 -0300

    feat: Agrega una función para obtener un código hex aleatorio y otra para obtener una paleta aleatoria

--- js/script.js
+++ js/script.js
@@ -2,26 +2,44 @@ const btnGenerator = document.getElementById("btn-generator");
 
 const paletteContainer = document.querySelector(".palette-container");
 
-const charactersHex = "ABCDEF0123456789"
-let hexColor = "#";
-
-for (let i = 0; i < 6; i++) {
-    let randomIndex = getRandomNumber(0, charactersHex.length-1);
-
-    hexColor+= charactersHex[randomIndex];    
-}
-
-
-const boxColor = document.createElement("div");
-boxColor.className = "box-color";
-boxColor.innerText = hexColor;
-boxColor.style.backgroundColor = hexColor;
-paletteContainer.appendChild(boxColor);
-
-
 
 
 function getRandomNumber(min, max) {
    return Math.floor(Math.random() * (max - min +1) + min)
 }
+
+function getRandomColor() {
+    const charactersHex = "ABCDEF0123456789"
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+        let randomIndex = getRandomNumber(0, charactersHex.length-1);
+
+        hexColor+= charactersHex[randomIndex];    
+    }
+
+    return hexColor;
+}
+
+function getPalette() {
+    console.log("El botón funciona bien");
+    paletteContainer.innerText = " ";
+    const paletteSize = document.getElementById("palette-size").value;
+    console.log(`cantidad de colores: ${paletteSize}`);
+
+    for (let i = 0; i < paletteSize; i++) {
+        const boxColor = document.createElement("div");
+        boxColor.className = "box-color";
+        boxColor.innerText = getRandomColor();
+        boxColor.style.backgroundColor = boxColor.innerText;
+        paletteContainer.appendChild(boxColor);    
+    
+    }
+    
+}
+
+btnGenerator.addEventListener("click", getPalette);
+
+
+
 
